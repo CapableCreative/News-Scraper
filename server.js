44b046042780fda76dc9f84fc37886c94d49cdd3
Stephen Fox <stephen@capablecreative.com>
2019-12-02 08:55:44 -0600

    Update to gitignore (node_modules)

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var port = process.envPORT || 3000;
+
+var app = express();
+
+app.use( logger("dev") );
+app.use( bodyParser.urlencoded({extended: false}) );
+app.engine( "handlbars", exphbs({ defaultLaout: "main" }));
+app.set( "view engine", "handlebars" );
+
+app.listen(port, function() {
+    console.log(`App listeing on port ${port}`);
+});
